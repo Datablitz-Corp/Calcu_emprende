6652a2b55084db0996b9e56676f71527d0465b74
Eliza-2202 <elizabeth@datablitz.com.pe>
2025-06-23 22:22:55 -0500

    view_2

--- frontend/react_app/src/pages/DetalleNegocio.js
+++ frontend/react_app/src/pages/DetalleNegocio.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
-import Layout from "./Layout";
+import Layout from "../components/Layout";
 
 function DetalleNegocio() {
   const { negocioId } = useParams();
